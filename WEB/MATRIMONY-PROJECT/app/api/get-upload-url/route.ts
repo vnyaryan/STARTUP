ef547df62a5f6bbc32e575a85b8242c84fb7becd
VINAY ARYA <vny.aryan@gmail.com>
2025-03-29 14:19:49 +0530

    Update route.ts

--- WEB/MATRIMONY-PROJECT/app/api/get-upload-url/route.ts
+++ WEB/MATRIMONY-PROJECT/app/api/get-upload-url/route.ts
@@ -1,22 +1,15 @@
-
 import { put } from "@vercel/blob";
 import { type NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
   const { filename, contentType } = await request.json();
 
-const blob = await put(`profile-pictures/${filename}`, request.body, {
-  access: "public",
-  contentType,
-});
-
-return NextResponse.json({
-  blobUrl: blob.url,
-});
-
+  const blob = await put(`profile-pictures/${filename}`, request.body, {
+    access: "public",
+    contentType,
+  });
 
   return NextResponse.json({
-    uploadUrl,
-    blobUrl: url,
+    blobUrl: blob.url,
   });
 }
