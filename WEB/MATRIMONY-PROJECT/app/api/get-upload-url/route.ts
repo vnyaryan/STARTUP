e09ebb2e6b21672479c2e030bc3dbf4c0295ff0e
VINAY ARYA <vny.aryan@gmail.com>
2025-03-30 10:37:03 +0530

    Update route.ts

--- WEB/MATRIMONY-PROJECT/app/api/get-upload-url/route.ts
+++ WEB/MATRIMONY-PROJECT/app/api/get-upload-url/route.ts
@@ -1,22 +1,13 @@
-import { generateUploadUrl } from "@vercel/blob";
+import { put } from "@vercel/blob";
 import { type NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
   const { filename, contentType } = await request.json();
 
-  // Generate secure presigned upload URL
-  const { url, token } = await generateUploadUrl();
+  const blob = await put(filename, null, { access: "public", contentType });
 
-  // Construct upload URL with required parameters
-  const uploadUrl = new URL(url);
-  uploadUrl.searchParams.set("token", token);
-  uploadUrl.searchParams.set("filename", filename);
-  uploadUrl.searchParams.set("contentType", contentType);
-
-  // Return the uploadUrl for file upload (PUT method)
-  // and the blobUrl to access/view the image
   return NextResponse.json({
-    uploadUrl: uploadUrl.toString(),
-    blobUrl: url,
+    uploadUrl: blob.uploadUrl,
+    blobUrl: blob.url,
   });
 }
