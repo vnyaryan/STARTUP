72cf9757e9388659700f021b6e39148d9398b452
VINAY ARYA <vny.aryan@gmail.com>
2025-04-06 16:25:54 +0530

    u

---
+++ WEB/matrimony-app/tailwind.config.js
@@ -0,0 +1,5 @@
+content: [
+  "./pages/**/*.{js,ts,jsx,tsx}",
+  "./components/**/*.{js,ts,jsx,tsx}",
+  "./app/**/*.{js,ts,jsx,tsx}"
+]
--- WEB/matrimony-old/middleware.ts
+++
@@ -1,9 +0,0 @@
-// Temporarily disable middleware to test API routes
-export const config = {
-  matcher: [], // Empty matcher means middleware won't run on any routes
-};
-
-export default function middleware() {
-  // This won't run due to empty matcher
-  return;
-}
